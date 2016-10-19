ed9abd34d60bc1d519ed733ca66909c0e43fdc87
Robert Johansson <hellorobjo@gmail.com>
2016-10-19 23:08:57 +0200

    Moved main loop out from the engine. The client must now first invoke engine.initialize() and then engine.evolve() each time it wants a new generation. Added engine.population_iterator() so the client has easy access to the individuals for simulation Removed engine.for_each_custom_call()

--- gengine.py
+++ gengine.py
@@ -4,81 +4,68 @@ import string
 import time
 from utils import constrain
 
-class NullIndividual:
-    def __init__(self):
-        pass
-
-    def set_individual(self, ind):
-        pass
-
-    def action(self):
-        pass
-
-class Individual:
-    def __init__(self, engine=None, genes=None):
-
-        self.custom_object = None
-
-        if engine is None:
-            raise RuntimeError('Engine not set')
-
-        self.engine = engine
-        if genes is None:
-            self.genes = []
-        else:
-            self.genes = genes[:]
-        self.fitness = 0
-
-    def get_genes_length(self):
-        return len(self.genes)
-
-    def set_custom_object(self, obj):
-        self.custom_object = obj
-
-    def get_genes(self):
-        return self.genes
-
-    def get_gene(self, index):
-        return self.genes[index]
-
-    def set_fitness(self, fitness):
-        if fitness < 0 or fitness > 1:
-            raise RuntimeError('Fitness must be in range [0, 1] was', fitness)
-        self.fitness = fitness
-
-    def mutate(self, probability):
-        new_genes = []
-        for i in range(len(self.genes)):
-            if random.random() < probability:
-                new_genes.append(self.engine.client.create_gene())
-            else:
-                new_genes.append(self.genes[i])
-        self.genes = new_genes
-
-    def __str__(self):
-        return '(I {} {})'.format(', '.join([str(x) for x in self.genes]), self.fitness)
-
 
 class ElementWiseCombinator:
+    """
+    Combinator that for each gene randomly either takes it from parent 1 or
+    parent 2.
+    """
     def combine(self, p1, p2):
         combined_genes = []
-        p1g = p1.get_genes()
-        p2g = p2.get_genes()
-        for i in range(len(p1g)):
+        p1g = p1.get_dna()
+        p2g = p2.get_dna()
+        for g1, g2 in zip(p1g, p2g):
             if random.random() < 0.5:
-                combined_genes.append(p1g[i])
+                combined_genes.append(g1)
             else:
-                combined_genes.append(p2g[i])
+                combined_genes.append(g2)
         return combined_genes
 
 
 class RandomBreakpointCombinator:
+    """
+    Combinator that takes a sequence of the DNA from parent 1 and the rest of
+    the sequence from parent 2.
+    """
     def combine(self, p1, p2):
         combined_genes = []
-        p1g = p1.get_genes()
-        p2g = p2.get_genes()
+        p1g = p1.get_dna()
+        p2g = p2.get_dna()
         n = random.randint(0, len(p1g) - 1)
         return p1g[0:n] + p2g[n:]
+
+
+class RandomParentCombinator:
+    """
+    Combinator that selects the complete DNA from either parent 1 or 2.
+    """
+    def combine(self, p1, p2):
+        if random.random() < 0.5:
+            return p1.get_dna()
+        else:
+            return p2.get_dna()
+
+
+class BaseIndividualMixin:
+    """
+    Objects that have a DNA and are part of the simulation should inherit or
+    mixin this class.
+    """
+    def __init__(self):
+        self.dna = None
+        self.fitness = 0
+
+    def set_fitness(self, fitness):
+        self.fitness = fitness
+
+    def get_fitness(self):
+        return self.fitness
+
+    def set_dna(self, dna):
+        self.dna = dna
+
+    def get_dna(self):
+        return self.dna
 
 
 class Population:
@@ -91,11 +78,11 @@ class Population:
     def add(self, ind):
         self.individuals.append(ind)
 
-    def get_individuals(self):
-        return self.individuals
+    def iterator(self):
+        return iter(self.individuals)
 
     def set_individuals(self, individuals):
-        self.individuals = individuals[:]
+        self.individuals = list(individuals)
 
     def select_individual(self):
         """
@@ -105,7 +92,7 @@ class Population:
         # In that case, just pick any
         allzero = True
         for ind in self.individuals:
-            if ind.fitness > 0:
+            if ind.get_fitness() > 0:
                 allzero = False
 
         if allzero:
@@ -124,10 +111,6 @@ class Population:
         # so just pick any.
         return self.individuals[random.randint(0, len(self.individuals) - 1)]
 
-    def for_each_custom_call(self, fn):
-        for ind in self.individuals:
-            fn(ind.custom_object)
-
     def get_size(self):
         return len(self.individuals)
 
@@ -143,24 +126,18 @@ class Engine:
         # ... code here ...
 
         self.client = client
-        self.stop_requested = False
         self.generation = 1
         self.population = None
         self.combinator = ElementWiseCombinator()
         self.pop_size = 3
-        self.gene_size = 1
         self.mutate_probability = 0.01
+        self.initialized = False
 
     def _get_configuration(self):
         config = self.client.get_configuration()
-        # TODO: Rewrite this more elegantly
+        # TODO: Rewrite this more elegantly and less verbose ...
         try:
             self.pop_size = config['population_size']
-        except:
-            pass
-
-        try:
-            self.gene_size = config['dna_size']
         except:
             pass
 
@@ -168,12 +145,6 @@ class Engine:
             self.mutate_probability = config['mutation_p']
         except:
             pass
-
-    def request_stop(self):
-        """
-        A client can call this method to request that the engine stops.
-        """
-        self.stop_requested = True
 
     def set_mutation_probability(self, p):
         """
@@ -183,47 +154,44 @@ class Engine:
         return self.mutate_probability
 
     def get_mutation_probability(self):
+        """
+        Returns the current mutation probability
+        """
         return self.mutate_probability
 
-    def get_gene_size(self):
-        """
-        Returns the number of genes in each individual's DNA
-        """
-        return self.gene_size
-
-    def _populate_random(self, pop_size, gene_size):
+    def _populate(self, pop_size):
         if self.population is not None:
             raise RuntimeError('Populate can only be called once')
 
         self.population = Population(self)
-        self.gene_size = gene_size
 
         for i in range(pop_size):
-            genes = []
-            for j in range(gene_size):
-                genes.append(self.client.create_gene())
-            self.population.add(self._create_individual(genes))
-
-    def _create_individual(self, new_genes):
-        obj = self.client.create_individual()
-        ind = Individual(self, new_genes)
-        ind.set_custom_object(obj)
-        obj.set_individual(ind)
-        return ind
+            dna = self.client.create_dna()
+            ind = self.client.create_individual()
+            ind.set_dna(dna)
+            self.population.add(ind)
 
     def _select_parents(self):
         p1 = self.population.select_individual()
         p2 = self.population.select_individual()
         return p1, p2
 
+    def _mutate(self, dna, probability):
+        new_dna = list(dna)
+        for i in range(len(dna)):
+            if random.random() < probability:
+                new_dna = self.client.mutate_dna(new_dna)
+        return new_dna
+
     def _evolve(self):
         new_individuals = []
 
         for i in range(self.population.get_size()):
             p1, p2 = self._select_parents()
-            new_genes = self.combinator.combine(p1, p2)
-            new_individual = self._create_individual(new_genes)
-            new_individual.mutate(self.mutate_probability)
+            new_dna = self.combinator.combine(p1, p2)
+            new_dna = self._mutate(new_dna, self.mutate_probability)
+            new_individual = self.client.create_individual()
+            new_individual.set_dna(new_dna)
             new_individuals.append(new_individual)
 
         self.population.set_individuals(new_individuals)
@@ -231,19 +199,20 @@ class Engine:
     def set_combinator(self, combinator):
         """
         A client can set a custom combinator object. The combinator object
-        must have a combine() method which the engine calls to combie two
-        parents to create a new individual for the next generation.
-        The combine() function should take to parent individuals and return a
-        vector of genes.
+        must have a combine() method which the engine calls to combie the DNA
+        of two parents and returns a combined DNA.
+        The combine() function should take two objects of that inherit from
+        BaseIndividualMixin and return a list of genes.
         """
         self.combinator = combinator
 
-    def for_each_custom_call(self, fn):
+    def population_iterator(self):
         """
-        A client can use this to call a function for each individual in the
-        population. The individual is sent as parameter.
+        Returns an iterator to the list of individuals in the population.
+        The types of each individual will match whatever is returned by
+        client.create_individual
         """
-        self.population.for_each_custom_call(fn)
+        return self.population.iterator()
 
     def _evaluate_all(self, engine):
         fitness_list = []
@@ -251,7 +220,7 @@ class Engine:
 
         # Collect fitness value for each individual as well as
         # find maximum fitness in the population
-        for ind in self.population.get_individuals():
+        for ind in self.population_iterator():
             fitness = self.client.evaluate_fitness(ind)
 
             if fitness < 0:
@@ -268,35 +237,49 @@ class Engine:
 
         # Normalize fitness to range [0, 1]
         i = 0
-        for ind in self.population.get_individuals():
+        for ind in self.population_iterator():
             fitness_list[i] /= max_fitness
             ind.set_fitness(fitness_list[i])
             i += 1
-        #print('\n'.join(['{:.5f}'.format(x) for x in reversed(sorted(fitness_list))]))
 
-    def run(self):
+    def initialize(self):
         """
-        This is the entry point for the simulation.
+        The application should call this prior to calling any other method in
+        the engine. It will call on_init() and generate an initial population.
         """
-        self.client.on_init(self)
-        self._get_configuration()
-        self._populate_random(self.pop_size, self.gene_size)
-        while True:
-            self.client.on_new_generation(self.generation)
-            self._evaluate_all(self)
-            self.client.on_evaluated(self.generation)
-            if self.stop_requested:
-                break
-            self._evolve()
-            self.generation += 1
+        if not self.initialized:
+            self.initialized = True
+            self.client.on_init(self)
+            self._get_configuration()
+            self._populate(self.pop_size)
+            self.client.on_new_population(self.generation)
+        else:
+            raise RuntimeError("Engine already initialized")
+
+
+    def evolve(self):
+        """
+        The client should call this every time it wishes to have a new
+        generation of the population generated.
+        """
+        if not self.initialized:
+            raise RuntimeError("Engine not initialized")
+
+        self._evaluate_all(self)
+        self.client.on_evaluated(self.generation)
+        self._evolve()
+        self.generation += 1
+        self.client.on_new_population(self.generation)
+
 
 class BaseClient:
     """
     Clients of the engine should subclass and override the methods of this
     class. For meaningful operation, at least the following need to be
     overriden:
-      * create_gene
+      * create_dna
       * create_individual
+      * mutate
       * evaluate_fitness
     """
     def get_configuration(self):
@@ -306,18 +289,25 @@ class BaseClient:
         """
         return {}
 
-    def create_gene(self):
+    def create_dna(self):
         """
-        Called by the engine when it needs to generate a gene.
-        Should return a single gene.
+        Called by the engine when it needs to generate a new dna.
+        Should return a list of genes.
         """
-        raise NotImplementedError('You must subclass BaseClient')
+        raise NotImplementedError('You must implement create_dna')
+
+    def mutate_dna(self, dna):
+        """
+        Called by the engine when it needs to mutate dna.
+        Should return a mutated version of the dna
+        """
+        raise NotImplementedError('You must implement mutate_dna')
 
     def create_individual(self):
         """
-        Should return an object that uses the dna.
+        Should return a subclass of BaseIndividualMixin.
         """
-        raise NotImplementedError('You must subclass BaseClient')
+        raise NotImplementedError('You must implement create_individual')
 
     def on_init(self, engine):
         """
@@ -330,9 +320,6 @@ class BaseClient:
     def on_new_generation(self, generation):
         """
         Called by the engine when a new generation is ready.
-        Implement your simulation loop here. Return when you have finished
-        simulating this generation. You can have your event handling code in
-        here too in case of interactive applications.
         """
         pass
 
@@ -348,9 +335,13 @@ class BaseClient:
         """
         Should calculate and return the fitness of the individual.
         """
-        raise NotImplementedError('You must sublcass BaseClient')
+        raise NotImplementedError('You must implement evaluate_fitness')
 
 
+"""
+The following part of the file contains a small example and is not part of the
+library itself.
+"""
 class ExampleClient(BaseClient):
     """
     A very simple client that uses the engine.
@@ -367,33 +358,45 @@ class ExampleClient(BaseClient):
         self.engine = engine
 
     def get_configuration(self):
-        return {'population_size': 8,
-                'dna_size': len(self.target)}
+        return {'population_size': 8}
 
-    def on_new_generation(self, generation):
+    def on_new_population(self, generation):
         self.generation = generation
 
-    def create_gene(self):
-        return random.choice(string.ascii_lowercase + ' ')
+    def create_dna(self):
+        dna = []
+        for i in range(len(self.target)):
+            dna.append(random.choice(string.ascii_lowercase + ' '))
+        return dna
 
     def create_individual(self):
-        return NullIndividual()
+        return BaseIndividualMixin()
+
+    def mutate_dna(self, dna):
+        i = random.randint(0, len(dna) - 1)
+        dna[i] = random.choice(string.ascii_lowercase + ' ')
+        return dna
 
     def evaluate_fitness(self, ind):
         fitness = 2
-        genes = ind.get_genes()
+        genes = ind.get_dna()
+        print(genes)
         for i in range(len(genes)):
             if genes[i] == self.target[i]:
                 fitness *= fitness
 
-        if ind.get_genes() == self.target:
+        if ind.get_dna() == self.target:
             self.solution_found = True
-            self.engine.request_stop()
             print('Found solution {} in {} generations.'.format(self.target, self.generation))
 
         return fitness
 
+    def is_done(self):
+        return self.solution_found
+
 if __name__ == '__main__':
     client = ExampleClient('to be or not to be')
     engine = Engine(client)
-    engine.run()
+    engine.initialize()
+    while not client.is_done():
+        engine.evolve()
